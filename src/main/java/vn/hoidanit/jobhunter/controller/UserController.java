941c2a4db735e307fa23f0fc12861b826a5548e1
Gianggg <dqgiang6@gmail.com>
2026-05-29 16:43:39 +0700

    get all user and update user

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,10 +1,13 @@
 package vn.hoidanit.jobhunter.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserServices;
@@ -29,5 +32,20 @@ public class UserController {
     public String deleteUser(@PathVariable("id") Long id) {
         this.userServices.handleDeleteUser(id);
         return "Delete user successfully";
+    }
+
+    @GetMapping("/user")
+    public List<User> getAllUser() {
+        return this.userServices.fetchAllUser();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") Long id) {
+        return this.userServices.fetchGetUserById(id);
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user) {
+        return this.userServices.handleUpdateUser(user);
     }
 }
--- src/main/java/vn/hoidanit/jobhunter/service/UserServices.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserServices.java
@@ -3,6 +3,8 @@ package vn.hoidanit.jobhunter.service;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServices {
@@ -15,10 +17,33 @@ public class UserServices {
 
     public User handleCreateUser(User user) {
         return this.userRepository.save(user);
-
     }
 
     public void handleDeleteUser(Long id) {
         this.userRepository.deleteById(id);
+    }
+
+    public List<User> fetchAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public User fetchGetUserById(Long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        } else
+            return null;
+    }
+
+    public User handleUpdateUser(User user){
+        Optional<User> userOptional = this.userRepository.findById(user.getId());
+        if (userOptional.isPresent()) {
+            User existingUser = userOptional.get();
+            existingUser.setName(user.getName());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setPassword(user.getPassword());
+            return this.userRepository.save(existingUser);
+        } else
+            return null;
     }
 }
