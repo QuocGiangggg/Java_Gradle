c667e240b9267bc5d5533ac550ce5a0550f5a32e
Gianggg <dqgiang6@gmail.com>
2026-06-03 16:39:54 +0700

    video57 baove endpoint

--- src/main/java/vn/hoidanit/jobhunter/config/SercurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SercurityConfiguration.java
@@ -13,9 +13,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
+import org.springframework.security.config.Customizer;
 
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import com.nimbusds.jose.util.Base64;
@@ -38,13 +41,28 @@ public class SercurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(
                         authz -> authz
-                                .requestMatchers("/").permitAll()
-                                .anyRequest().permitAll()
+                                .requestMatchers("/", "/login").permitAll()
+                                .anyRequest().authenticated()
 
                 )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 .formLogin(f -> f.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(
+            getSecretKey()).macAlgorithm(SecurityUtil.JWT_ALGORITHM).build();
+        return token ->{
+            try {
+                return jwtDecoder.decode(token);
+            } catch(Exception e){
+                System.out.println(">>> jwtError: "+ e.getMessage());
+                throw e;
+            }
+        };
     }
 
     @Bean
@@ -55,6 +73,6 @@ public class SercurityConfiguration {
     private SecretKey getSecretKey() {
 
         byte[] keyBytes = Base64.from(jwtKey).decode();
-        return new SecretKeySpec(keyBytes, 0, keyBytes.length,SecurityUtil.JWT_ALGORITHM.getName());
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length, SecurityUtil.JWT_ALGORITHM.getName());
     }
 }
