1f41e3ef9bdad4ece936c514d1c2e81b2ef4df82
Gianggg <dqgiang6@gmail.com>
2026-06-10 11:28:17 +0700

    video82

--- src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -4,15 +4,22 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 import vn.hoidanit.jobhunter.domain.dto.LoginDTO;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
 import vn.hoidanit.jobhunter.domain.dto.ResLoginDTO;
+import vn.hoidanit.jobhunter.service.UserServices;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import vn.hoidanit.jobhunter.domain.User;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,10 +27,13 @@ public class AuthController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final SecurityUtil securityUtil;
+    private final UserServices userServices;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil,
+            UserServices userServices) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.securityUtil = securityUtil;
+        this.userServices = userServices;
     }
 
     @PostMapping("/login")
@@ -40,7 +50,18 @@ public class AuthController {
         String access_token = this.securityUtil.createToken(authentication);
         SecurityContextHolder.getContext().setAuthentication(authentication);
         ResLoginDTO resLoginDTO = new ResLoginDTO();
+        User currentUserDB = this.userServices.handleGetUserByUsername(loginDTO.getUsername());
+
+        if (currentUserDB != null) {
+            ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(
+                    currentUserDB.getId(),
+                    currentUserDB.getEmail(),
+                    currentUserDB.getName());
+            resLoginDTO.setUser(userLogin);
+        }
+
         resLoginDTO.setAccessToken(access_token);
+
         return ResponseEntity.ok().body(resLoginDTO);
     }
 }
--- src/main/java/vn/hoidanit/jobhunter/domain/dto/ResLoginDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/dto/ResLoginDTO.java
@@ -1,7 +1,9 @@
 package vn.hoidanit.jobhunter.domain.dto;
 
 public class ResLoginDTO {
+
     private String accessToken;
+    private UserLogin user;
 
     public String getAccessToken() {
         return accessToken;
@@ -11,4 +13,52 @@ public class ResLoginDTO {
         this.accessToken = accessToken;
     }
 
+    public UserLogin getUser() {
+        return user;
+    }
+
+    public void setUser(UserLogin user) {
+        this.user = user;
+    }
+
+    public static class UserLogin {
+        private long id;
+        private String email;
+        private String name;
+
+        public UserLogin() {
+        }
+
+        public UserLogin(long id, String email, String name) {
+            this.id = id;
+            this.email = email;
+            this.name = name;
+        }
+
+        public long getId() {
+            return id;
+
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
 }
