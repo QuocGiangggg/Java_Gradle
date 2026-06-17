9574fc7ab3f59705118c0b04b59cf3f8e8281c93
Gianggg <dqgiang6@gmail.com>
2026-06-17 17:12:16 +0700

    video 100

--- src/main/java/vn/hoidanit/jobhunter/config/StaticResourcesWebConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/StaticResourcesWebConfiguration.java
@@ -8,12 +8,11 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 @Configuration
 public class StaticResourcesWebConfiguration implements WebMvcConfigurer {
 
-    @Value("${hoidanit.upload-file.base-path}")
-    private String basePath;
+    @Value("${hoidanit.upload-file.base-uri}")
+    private String baseURI;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/storage/**").addResourceLocations(basePath);
-
+        registry.addResourceHandler("/storage/**").addResourceLocations(baseURI);
     }
 }
